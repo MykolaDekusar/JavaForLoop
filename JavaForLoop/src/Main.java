84aa8f6f49935d700781dcde5f01595185a6ec79
MykolaDekusar <ndekusar97@gmail.com>
2024-10-27 13:55:25 +0100

    Use of FOR to cycle 10 times

--- JavaForLoop/src/Main.java
+++ JavaForLoop/src/Main.java
@@ -2,7 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//il loop FOR esegue il codice per un tot di volte occhio alla condizione
+		//che potrebbe non verificarsi mai e rendere il loop infinito
+		//inizializzo i, verifico se è minore di 10, aumento i di 1 alla fine del ciclo
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
